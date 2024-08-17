bd92830b82020aed7d420f0938b1ee13d82c7ed0
Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>
2024-08-17 23:58:45 +0500

    Update EnigmaDecode.py
    Signed-off-by: Mohsin Mukhtiar Lashari <160683327+president-xd@users.noreply.github.com>

--- src/EnigmaDecode.py
+++ src/EnigmaDecode.py
@@ -3,6 +3,20 @@ import urllib.parse
 # Define Base32 characters
 BASE32_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
 
+def banner():
+    print('''
+
+ 88888888b                   oo                              888888ba                                   dP          
+ 88                                                          88    `8b                                  88          
+a88aaaa    88d888b. .d8888b. dP 88d888b. 88d8b.d8b. .d8888b. 88     88 .d8888b. .d8888b. .d8888b. .d888b88 .d8888b. 
+ 88        88'  `88 88'  `88 88 88'  `88 88'`88'`88 88'  `88 88     88 88ooood8 88'  `"" 88'  `88 88'  `88 88ooood8 
+ 88        88    88 88.  .88 88 88    88 88  88  88 88.  .88 88    .8P 88.  ... 88.  ... 88.  .88 88.  .88 88.  ... 
+ 88888888P dP    dP `8888P88 dP dP    dP dP  dP  dP `88888P8 8888888P  `88888P' `88888P' `88888P' `88888P8 `88888P' 
+                         .88                                                                                        
+                     d8888P                                                                                         
+By President
+
+    ''')
 def binary_decode(char: str) -> str:
     """Returns the decrypted text for Binary."""
     if " " not in char and len(char) > 8:
